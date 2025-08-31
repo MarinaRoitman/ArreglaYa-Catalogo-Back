554ad449cae108a4332450aca0a8c867cbeaa82e
Oriana C. <73916359+OriBianchi@users.noreply.github.com>
2025-08-31 15:42:07 -0300

    Update security.py

--- api/core/security.py
+++ api/core/security.py
@@ -5,7 +5,7 @@ import os
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
