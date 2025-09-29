ad5477488e5e0b7b14a17cb6ff75c4cbbd7137b3
Oriana C. <73916359+OriBianchi@users.noreply.github.com>
2025-09-28 21:03:38 -0300

    Change default database name to 'catalogo-dev'

--- api/core/database.py
+++ api/core/database.py
@@ -17,7 +17,7 @@ db_config = {
     "port": int(os.getenv("DB_PORT", 3306)),
     "user": os.getenv("MYSQL_USER"),
     "password": os.getenv("MYSQL_PASSWORD"),
-    "database": os.getenv("MYSQL_DATABASE", "catalogo"),
+    "database": os.getenv("MYSQL_DATABASE", "catalogo-dev"),
 }
 
 @contextlib.contextmanager
